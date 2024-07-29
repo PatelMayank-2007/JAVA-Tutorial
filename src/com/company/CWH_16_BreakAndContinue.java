196bf9a39e1c409f757d64186fc90e56adbd3451
mayank <patelmayank232007@gmail.com>
2024-07-29 14:44:35 +0530

    first commit

--- src/com/company/CWH_16_BreakAndContinue.java
+++ src/com/company/CWH_16_BreakAndContinue.java
@@ -2,7 +2,36 @@ package com.company;
 
 public class CWH_16_BreakAndContinue {
     public static void main(String[] args) {
-        System.out.println("Mayank");
+
+               // break statement
+
+//        for(int i = 0;i<=50;i++){
+//            System.out.println(i);
+//            System.out.println("i am mayank");
+//            if (i==2){
+//                System.out.println("Ending the loop.");
+//                break;
+//            }
+//        }
+//        int i= 0 ;
+//        while(i<=5){
+//            System.out.println(i);
+//            System.out.println("i am mayank");
+//
+//        if (i == 2) {
+//            System.out.println("Ending of loop");
+//            break;}
+//        i++;
+//        }
+
+         //continue statement
+
+        for (int i=0;i<=50;i++){
+            if(i == 3)continue;
+            System.out.print(i);
+            System.out.println("i aM mayank");
+        }
+
 
     }
 
