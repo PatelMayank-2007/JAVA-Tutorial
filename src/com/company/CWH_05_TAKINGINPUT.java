e6dc126493f28b98cece6e2c43f58410168d25fd
mayank <patelmayank232007@gmail.com>
2024-07-29 11:30:20 +0530

    first commit

---
+++ src/com/company/CWH_05_TAKINGINPUT.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class CWH_05_TAKINGINPUT {
+    public static void main (String[] args) {
+        System.out.println("Taking input from user");
+        Scanner sc = new Scanner (System.in);
+        System.out.print("enter your first number:" );
+        //int a =sc.nextInt();
+        float a = sc.nextFloat();
+        System.out.print("enter your second number");
+        //int b =sc.nextInt();
+        float b= sc.nextFloat();
+        //int sum =a+b;
+        float sum=a+b;
+        System.out.print("your answer is: ");
+        System.out.println(sum);
+    }
+}
