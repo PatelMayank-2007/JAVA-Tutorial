eeaa57fe8663d2daa9e623524ff17829d7da0483
mayank <patelmayank232007@gmail.com>
2024-07-29 11:24:49 +0530

    first commit

---
+++ src/com/company/CWH_04_LITERALS.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class CWH_04_LITERALS {
+    public static void main(String[] args) {
+            byte age = 32;
+            short age2 = 43;
+            int age3 = 23;
+            long age4 = 343434343433L;
+            char ch ='K';
+            float age5 = 5.6f;
+            double age6 = 2.3;
+            boolean a = true;
+                    System.out.println(age6 );
+
+    }
+}
