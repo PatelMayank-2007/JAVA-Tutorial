a17d71f5223083c3ec3865f794d3c0c0935092fc
mayank <patelmayank232007@gmail.com>
2024-08-06 23:50:41 +0530

    first commit

--- src/com/company/CWH_10_Conditionals.java
+++ src/com/company/CWH_10_Conditionals.java
@@ -4,7 +4,7 @@ public class CWH_10_Conditionals {
     public static void main(String[] args) {
         Scanner M = new Scanner(System.in);
         System.out.println("Enter your age here:");
-        int age = M.nextInt();
+        int age = M.nextInt();  
         if (age >= 18) {
              System.out.println("you are in adult category ");
         }
---
+++ src/com/company/CWHpractice_set_05.java
@@ -0,0 +1,103 @@
+package com.company;
+import java.util.Scanner;
+public class CWHpractice_set_05 {
+    public static void main(String[] args) {
+        //Question 1  Write a program to print the following pattern :**** *** ** *
+//       for(int i = 50;i>0;--i){
+//           for(int j=0;j<i;j++){
+//               System.out.print("*");
+//           }
+//           System.out.print("\n");
+//       }
+
+        // Question 2 Write a program to sum first n even numbers using a while loop.
+//        int sum = 0;
+//        Scanner M = new Scanner(System.in);
+//        System.out.println("Enter the number:");
+//        int n = M.nextInt();
+//
+//        int i = 0;
+//        while(i<=n){
+//            sum =sum + 2*i;
+//            i++;
+//        }
+//        System.out.print("The sum of " + n + " even number is " + sum);
+
+        // Question 3 : Write a program to print the multiplication table of a given number n.
+//        Scanner M = new Scanner(System.in);
+//        System.out.print("Enter the number for which you have to write table:");
+//        int n = M.nextInt();
+//        for(int i=1;i<=10;i++){
+//            System.out.printf("%d X %d = %d", n, i, i*n);
+//            System.out.print("\n");
+//        }
+
+        // Question 4 :Write a program to print a multiplication table of 10 in reverse order.
+//        int n = 10;
+//        for (int i=10;i>=1;--i){
+//            System.out.printf("%d X %d = %d", n, i, i*n);
+//            System.out.print("\n");
+//
+//        }
+
+        //Question 5: Write a program to find the factorial of a given number using for loops.
+//        Scanner M = new Scanner(System.in);
+//        System.out.print("Enter the number for which you have to find factorial:");
+//        int n = M.nextInt();
+//        double mul = 1D;
+//        for(int i=n;i>=2;i--){
+//            mul = mul *i;
+//            System.out.println(mul);
+//        }
+
+        //Question 6: solve question 5 using while loop.
+//        Scanner M = new Scanner(System.in);
+//        System.out.print("Enter the number for which you have to find factorial:");
+//        int n = M.nextInt();
+//        int mul = 1;
+//        int i = n;
+//        while(i>=1){
+//           mul= mul*i;
+//            i--;
+//        }
+//        System.out.println(mul);
+
+        //Question 8:What can be done using one type of loop can also be done using the other two types of loops - True or False.
+        // true
+
+        //Question 9:Write a program to calculate the sum of the numbers occurring in the multiplication table of n.
+//        Scanner M = new Scanner(System.in);
+//        System.out.print("Enter the number:");
+//        int n = M.nextInt();
+//        int sum = 0;
+//        for (int i = 1;i<=10;i++){
+//            sum+=n*i;
+//        }
+//        System.out.println(sum);
+
+        // Question 11 : Repeat problem 2 using for loop.
+//        int sum = 0;
+//        Scanner M = new Scanner(System.in);
+//        System.out.println("Enter the number:");
+//        int n = M.nextInt();
+//
+//        for(int i=0;i<=n;i++){
+//            sum =sum + 2*i;
+//
+//        }
+//        System.out.print("The sum of " + n + " even number is " + sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
