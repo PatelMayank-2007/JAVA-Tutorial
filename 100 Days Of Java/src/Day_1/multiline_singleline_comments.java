3405867e29ee7dbe6d1b77c4f106e894a40dd15c
mayank <patelmayank232007@gmail.com>
2024-12-24 17:14:21 +0530

    Day1

---
+++ 100 Days Of Java/src/Day_1/multiline_singleline_comments.java
@@ -0,0 +1,12 @@
+package Day_1;
+
+public class multiline_singleline_comments {
+    public static void main(String[] args) {
+        //this is single line comment.
+    /* this is a multiline comment.
+    Its end with same sign as we do in first one.
+     */
+        System.out.println("HELLO");
+
+    }
+}
