7a3c9fdf6803b0ac95b202b468e36b5d14256936
mayank <patelmayank232007@gmail.com>
2024-12-24 17:16:25 +0530

    declaring variable

---
+++ 100 Days Of Java/src/Day_2/declaring_variable.java
@@ -0,0 +1,22 @@
+package Day_2;
+
+public class declaring_variable {
+    public static void main(String[] args) {
+        byte age = 32;
+        short age2 = 43;
+        int age3 = 23;
+        long age4 = 343434343433L;
+        char ch ='K';
+        float age5 = 5.6f;
+        double age6 = 2.3;
+        boolean a = true;
+        System.out.println(age6);
+        System.out.println(age5);
+        System.out.println(age4);
+        System.out.println(age3);
+        System.out.println(age2);
+        System.out.println(age);
+        System.out.println(ch);
+
+    }
+}
